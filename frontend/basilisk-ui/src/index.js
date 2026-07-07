8062d7bc14a9970058531cbb04c08f552764421e
bodkr20 <bodkr2@gmail.com>
2026-07-08 01:12:43 +0300

    Update index.js

--- frontend/basilisk-ui/src/index.js
+++ frontend/basilisk-ui/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// 🔥 هذا السطر يحل مشكلة Invalid Host header
+window.location.hostname = 'basilisk-ui.onrender.com';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
